cd6b0497737f228aa3ebaf7ae181f8b2f52d7a81
Mario Krajacic <krajacic.mario@gmail.com>
2020-07-24 13:35:47 +0200

    Implementing proper endpoint methods.

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-class Student(Resource):
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404  # 404 status not found for server
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201    # 201 status created for server
 
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000)
