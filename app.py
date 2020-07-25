977d22a4af19be9e1771f7c5a1f6a5cdc29c6c09
Mario Krajacic <krajacic.mario@gmail.com>
2020-07-25 18:38:56 +0200

    Optimizing.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -13,6 +13,11 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True,
+                        help="This field cannot be blank!")
+    data = parser.parse_args()
+
     #@jwt_required()     # for authorisation purpose this decorator can be put on every method
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -22,7 +27,7 @@ class Item(Resource):
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{}' already exists." .format(name)}, 400
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201    # 201 status created for server
@@ -33,7 +38,7 @@ class Item(Resource):
         return {'message': 'Items deleted'}
     
     def put(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item:
             item.update(data)
